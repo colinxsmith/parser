03c5fdd80dbadef05505d8468c32c6662c6336a1
Colin Smith <colin.smith@corfinancialgroup.com>
2019-05-29 10:27:04 +0100

    Report risk too

--- checklog.js
+++ checklog.js
@@ -1,114 +1,92 @@
-const test = require("./build/Release/PARSER");
-const opt = require("../router/build/Release/OPT");
+const parseObj = require("./build/Release/PARSER");
+const optObj = require("../router/build/Release/OPT");
 // console.log(opt);
-/*
-extern "C"  short  Optimise_internalCVPAFblSaMSoft(dimen n,long nfac,char** stocknames,vector w_opt,dimen m,
-									vector AAA,vector L,vector U,vector alpha,
-									vector benchmark,vector QMATRIX,real gamma,vector Initial,
-									real delta,vector buy,vector sell,real kappa,long basket,
-									long trades,int revise,int costs,real min_holding,
-									real min_trade,
-									int m_LS,int Fully_Invested,real Rmin,real Rmax,
-									int m_Round,vector min_lot,vector size_lot,int* shake,
-									dimen ncomp,vector Composite,real LSValue,
-									dimen npiece,vector hpiece,vector pgrad,
-									dimen nabs,vector Abs_A,dimen mabs,dimen* I_A,vector Abs_U,
-									vector FFC,vector FLOAD,vector SSV,
-									double minRisk=-1,double maxRisk=-1,double* ogamma=0,
-									vector mask=0,int log=0,char* logfile=0,
-									int downrisk=0,double downfactor=3.0,
-									long longbasket=-1,long shortbasket=-1,
-									long tradebuy=-1,long tradesell=-1,
-									double zetaS=1,double zetaF=1,double ShortCostScale=1,
-									double LSValuel=0,vector Abs_L=0,vector shortalphacost=0,int never_slow=0,size_t*mem_kbytes=0,
-									dimen soft_m=0,vector soft_l=0,vector soft_b=0,
-                                    vector soft_L=0,vector soft_U=0,vector soft_A=0);
-*/
-const DATA = new test.DoubleMap();
+
+const DATA = new parseObj.DoubleMap();
 
 const line_len = 50000;
 let read = '';
 for (let i = 0; i < line_len; ++i) {
     read += 'a';
 }
-const fwords = new test.StringVector(),
+const fwords = new parseObj.StringVector(),
     space = ' ';
 const keys = 'n nfac m A L names soft_m soft_A soft_L soft_U soft_b soft_l U alpha shortalphacost bench Q SV FL FC gamma initial delta buy sell qbuy qsell kappa basket longbasket downrisk downfactor shortbasket tradebuy tradesell tradenum revise costs min_holding min_trade ls full minRisk maxRisk rmin rmax round min_lot size_lot ncomp Composites value valuel npiece hpiece pgrad nabs A_abs mabs I_A Abs_U Abs_L ShortCostScale mask issues five ten forty';
 const scalars = 'n nfac m soft_m gamma delta kappa basket revise costs rmin longbasket downrisk downfactor shortbasket tradebuy tradesell tradenum value valuel npiece rmax minRisk maxRisk ls full min_trade min_holding round ncomp nabs mabs five ten forty ShortCostScale';
-test.Parser('run.log', keys, read, line_len, fwords, DATA, space);
+parseObj.Parser('run.log', keys, read, line_len, fwords, DATA, space);
 keys.split(' ').forEach(kk => {
     const sss = scalars.split(' ');
     if (sss.includes(kk)) {
-        console.log('scalar', kk, +test.gets(DATA, kk));
+        console.log('scalar', kk, +parseObj.gets(DATA, kk));
     } else {
-        console.log(kk, (test.getv(DATA, kk)) === undefined ? 'blank' : 'length ' + (test.getv(DATA, kk)).length);
+        console.log(kk, (parseObj.getv(DATA, kk)) === undefined ? 'blank' : 'length ' + (parseObj.getv(DATA, kk)).length);
     }
 });
 
 console.log(fwords.size(), keys.split(' ').length);
 
-const n = +test.geti(DATA, 'n');
-const nfac = +test.gets(DATA, 'nfac');
-const names = test.getv(DATA, 'names');
-const m = +test.geti(DATA, 'm');
-const A = test.getv(DATA, 'A') === undefined ? [] : test.getv(DATA, 'A');
-const L = test.getv(DATA, 'L') === undefined ? [] : test.getv(DATA, 'L');
-const soft_m = +test.geti(DATA, 'soft_m');
-const soft_A = test.getv(DATA, 'soft_A') === undefined ? [] : test.getv(DATA, 'soft_A');
-const soft_L = test.getv(DATA, 'soft_L') === undefined ? [] : test.getv(DATA, 'soft_L');
-const soft_U = test.getv(DATA, 'soft_U') === undefined ? [] : test.getv(DATA, 'soft_U');
-const soft_b = test.getv(DATA, 'soft_b') === undefined ? [] : test.getv(DATA, 'soft_b');
-const soft_l = test.getv(DATA, 'soft_l') === undefined ? [] : test.getv(DATA, 'soft_l');
-const U = test.getv(DATA, 'U') === undefined ? [] : test.getv(DATA, 'U');
-const alpha = test.getv(DATA, 'alpha') === undefined ? [] : test.getv(DATA, 'alpha');
-const shortalphacost = test.getv(DATA, 'shortalphacost') === undefined ? [] : test.getv(DATA, 'shortalphacost');
-const bench = test.getv(DATA, 'bench') === undefined ? [] : test.getv(DATA, 'bench');
-const Q = test.getv(DATA, 'Q') === undefined ? [] : test.getv(DATA, 'Q');
-const SV = test.getv(DATA, 'SV') === undefined ? [] : test.getv(DATA, 'SV');
-const FL = test.getv(DATA, 'FL') === undefined ? [] : test.getv(DATA, 'FL');
-const FC = test.getv(DATA, 'FC') === undefined ? [] : test.getv(DATA, 'FC');
-const gamma = +test.gets(DATA, 'gamma');
-const initial = test.getv(DATA, 'initial') === undefined ? [] : test.getv(DATA, 'initial');
-const delta = +test.gets(DATA, 'delta');
-const buy = test.getv(DATA, 'buy') === undefined ? [] : test.getv(DATA, 'buy');
-const sell = test.getv(DATA, 'sell') === undefined ? [] : test.getv(DATA, 'sell');
-const kappa = +test.gets(DATA, 'kappa');
-const basket = +test.geti(DATA, 'basket');
-const longbasket = +test.geti(DATA, 'longbasket');
-const downrisk = +test.gets(DATA, 'downrisk');
-const downfactor = +test.gets(DATA, 'downfactor');
-const shortbasket = +test.geti(DATA, 'shortbasket');
-const tradebuy = +test.geti(DATA, 'tradebuy');
-const tradesell = +test.geti(DATA, 'tradesell');
-const tradenum = +test.geti(DATA, 'tradenum');
-const revise = +test.geti(DATA, 'revise');
-const costs = +test.geti(DATA, 'costs');
-const min_holding = +test.gets(DATA, 'min_holding');
-const min_trade = +test.gets(DATA, 'min_trade');
-const ls = +test.geti(DATA, 'ls');
-const full = +test.gets(DATA, 'full');
-const minRisk = +test.gets(DATA, 'minRisk');
-const maxRisk = +test.gets(DATA, 'maxRisk');
-const rmin = +test.gets(DATA, 'rmin');
-const rmax = +test.gets(DATA, 'rmax');
-const round = +test.geti(DATA, 'round');
-const min_lot = test.getv(DATA, 'size_lot') === undefined ? [] : test.getv(DATA, 'size_lot');
-const size_lot = test.getv(DATA, 'size_lot') === undefined ? [] : test.getv(DATA, 'size_lot');
-const ncomp = +test.geti(DATA, 'ncomp');
-const Composites = test.getv(DATA, 'Composites') === undefined ? [] : test.getv(DATA, 'Composites');
-const value = +test.gets(DATA, 'value');
-const valuel = +test.gets(DATA, 'valuel');
-const npiece = +test.geti(DATA, 'npiece');
-const hpiece = test.getv(DATA, 'hpiece') === undefined ? [] : test.getv(DATA, 'hpiece');
-const pgrad = test.getv(DATA, 'pgrad') === undefined ? [] : test.getv(DATA, 'pgrad');
-const nabs = +test.gets(DATA, 'nabs');
-const A_abs = test.getv(DATA, 'A_abs') === undefined ? [] : test.getv(DATA, 'A_abs');
-const mabs = +test.gets(DATA, 'mabs');
-const I_A = test.getv(DATA, 'I_A') === undefined ? [] : test.getv(DATA, 'I_A');
-const Abs_U = test.getv(DATA, 'Abs_U') === undefined ? [] : test.getv(DATA, 'Abs_U');
-const Abs_L = test.getv(DATA, 'Abs_L') === undefined ? [] : test.getv(DATA, 'Abs_U');
-const ShortCostScale = +test.gets(DATA, 'ShortCostScale');
-const mask = test.getv(DATA, 'mask') === undefined ? [] : test.getv(DATA, 'mask');
+const n = +parseObj.geti(DATA, 'n');
+const nfac = +parseObj.geti(DATA, 'nfac');
+const names = parseObj.getv(DATA, 'names');
+const m = +parseObj.geti(DATA, 'm');
+const A = parseObj.getv(DATA, 'A') === undefined ? [] : parseObj.getv(DATA, 'A');
+const L = parseObj.getv(DATA, 'L') === undefined ? [] : parseObj.getv(DATA, 'L');
+const soft_m = +parseObj.geti(DATA, 'soft_m');
+const soft_A = parseObj.getv(DATA, 'soft_A') === undefined ? [] : parseObj.getv(DATA, 'soft_A');
+const soft_L = parseObj.getv(DATA, 'soft_L') === undefined ? [] : parseObj.getv(DATA, 'soft_L');
+const soft_U = parseObj.getv(DATA, 'soft_U') === undefined ? [] : parseObj.getv(DATA, 'soft_U');
+const soft_b = parseObj.getv(DATA, 'soft_b') === undefined ? [] : parseObj.getv(DATA, 'soft_b');
+const soft_l = parseObj.getv(DATA, 'soft_l') === undefined ? [] : parseObj.getv(DATA, 'soft_l');
+const U = parseObj.getv(DATA, 'U') === undefined ? [] : parseObj.getv(DATA, 'U');
+const alpha = parseObj.getv(DATA, 'alpha') === undefined ? [] : parseObj.getv(DATA, 'alpha');
+const shortalphacost = parseObj.getv(DATA, 'shortalphacost') === undefined ? [] : parseObj.getv(DATA, 'shortalphacost');
+const bench = parseObj.getv(DATA, 'bench') === undefined ? [] : parseObj.getv(DATA, 'bench');
+const Q = parseObj.getv(DATA, 'Q') === undefined ? [] : parseObj.getv(DATA, 'Q');
+const SV = parseObj.getv(DATA, 'SV') === undefined ? [] : parseObj.getv(DATA, 'SV');
+const FL = parseObj.getv(DATA, 'FL') === undefined ? [] : parseObj.getv(DATA, 'FL');
+const FC = parseObj.getv(DATA, 'FC') === undefined ? [] : parseObj.getv(DATA, 'FC');
+const gamma = +parseObj.gets(DATA, 'gamma');
+const initial = parseObj.getv(DATA, 'initial') === undefined ? [] : parseObj.getv(DATA, 'initial');
+const delta = +parseObj.gets(DATA, 'delta');
+const buy = parseObj.getv(DATA, 'buy') === undefined ? [] : parseObj.getv(DATA, 'buy');
+const sell = parseObj.getv(DATA, 'sell') === undefined ? [] : parseObj.getv(DATA, 'sell');
+const kappa = +parseObj.gets(DATA, 'kappa');
+const basket = +parseObj.geti(DATA, 'basket');
+const longbasket = +parseObj.geti(DATA, 'longbasket');
+const downrisk = +parseObj.gets(DATA, 'downrisk');
+const downfactor = +parseObj.gets(DATA, 'downfactor');
+const shortbasket = +parseObj.geti(DATA, 'shortbasket');
+const tradebuy = +parseObj.geti(DATA, 'tradebuy');
+const tradesell = +parseObj.geti(DATA, 'tradesell');
+const tradenum = +parseObj.geti(DATA, 'tradenum');
+const revise = +parseObj.geti(DATA, 'revise');
+const costs = +parseObj.geti(DATA, 'costs');
+const min_holding = +parseObj.gets(DATA, 'min_holding');
+const min_trade = +parseObj.gets(DATA, 'min_trade');
+const ls = +parseObj.geti(DATA, 'ls');
+const full = +parseObj.gets(DATA, 'full');
+const minRisk = +parseObj.gets(DATA, 'minRisk');
+const maxRisk = +parseObj.gets(DATA, 'maxRisk');
+const rmin = +parseObj.gets(DATA, 'rmin');
+const rmax = +parseObj.gets(DATA, 'rmax');
+const round = +parseObj.geti(DATA, 'round');
+const min_lot = parseObj.getv(DATA, 'size_lot') === undefined ? [] : parseObj.getv(DATA, 'size_lot');
+const size_lot = parseObj.getv(DATA, 'size_lot') === undefined ? [] : parseObj.getv(DATA, 'size_lot');
+const ncomp = +parseObj.geti(DATA, 'ncomp');
+const Composites = parseObj.getv(DATA, 'Composites') === undefined ? [] : parseObj.getv(DATA, 'Composites');
+const value = +parseObj.gets(DATA, 'value');
+const valuel = +parseObj.gets(DATA, 'valuel');
+const npiece = +parseObj.geti(DATA, 'npiece');
+const hpiece = parseObj.getv(DATA, 'hpiece') === undefined ? [] : parseObj.getv(DATA, 'hpiece');
+const pgrad = parseObj.getv(DATA, 'pgrad') === undefined ? [] : parseObj.getv(DATA, 'pgrad');
+const nabs = +parseObj.gets(DATA, 'nabs');
+const A_abs = parseObj.getv(DATA, 'A_abs') === undefined ? [] : parseObj.getv(DATA, 'A_abs');
+const mabs = +parseObj.gets(DATA, 'mabs');
+const I_A = parseObj.getv(DATA, 'I_A') === undefined ? [] : parseObj.getv(DATA, 'I_A');
+const Abs_U = parseObj.getv(DATA, 'Abs_U') === undefined ? [] : parseObj.getv(DATA, 'Abs_U');
+const Abs_L = parseObj.getv(DATA, 'Abs_L') === undefined ? [] : parseObj.getv(DATA, 'Abs_U');
+const ShortCostScale = +parseObj.gets(DATA, 'ShortCostScale');
+const mask = parseObj.getv(DATA, 'mask') === undefined ? [] : parseObj.getv(DATA, 'mask');
 const w = Array(n);
 const ogamma = Array(1);
 const shake = Array(n);
@@ -117,8 +95,8 @@ const logfile = 'opt.log';
 const zetaS = 1,
     zetaF = 1,
     never_slow = 0,
-    mem_kbytes = [];
-const back = opt.Optimise_internalCVPAFblSaMSoft(n, nfac, names, w, m, A, L, U, alpha, bench, Q,
+    mem_kbytes = [1];
+const back = optObj.Optimise_internalCVPAFblSaMSoft(n, nfac, names, w, m, A, L, U, alpha, bench, Q,
     gamma, initial, delta, buy, sell, kappa, basket, tradenum, revise, costs, min_holding, min_trade,
     ls, full, rmin, rmax, round, min_lot, size_lot, shake, ncomp, Composites, value,
     npiece, hpiece, pgrad, nabs, A_abs, mabs, I_A, Abs_U, FC, FL, SV, minRisk, maxRisk, ogamma,
@@ -159,4 +137,8 @@ w.forEach((d, i) => {
 console.log('Number of holdings', holdings);
 console.log('Minimum holding', minhold);
 console.log('gamma back', ogamma[0]);
-console.log(opt.Return_Message(back));
+console.log(optObj.Return_Message(back), mem_kbytes[0], 'kbytes');
+const arisk = [1], risk = [1], brisk = [1], pbeta = [1], Rrisk = [1];
+optObj.Get_RisksC(n, nfac, Q, w, 0, arisk, risk, Rrisk, brisk,
+    pbeta, ncomp, Composites);
+console.log('risk', risk[0]);
