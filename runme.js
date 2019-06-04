c6cf589be0859169bd4c9c51c916e3e4bcdd0272
Colin Smith <colin.smith@corfinancialgroup.com>
2019-06-04 16:06:56 +0100

    more tests

--- runme.js
+++ runme.js
@@ -12,7 +12,7 @@ for (let i = 0; i < line_len; ++i) {
 }
 var fwords = new test.StringVector(), space = ' ';
 test.Parser('paritysplit.log', 'n initial alpha', read, line_len, fwords, DATA, space);
-console.log(test.getv(DATA,'alpha'));
+console.log(test.getv(DATA, 'alpha'));
 let vec = Array(+test.geti(DATA, 'n'));
 console.log(vec);
 console.log(DATA.size(), fwords.size());
@@ -21,7 +21,19 @@ test.getvec(DATA, 'alpha', vec);
 console.log(vec);
 console.log(test.getv(DATA, 'alpha'));
 console.log(test.getv(DATA, 'n'));
-for (let i = 0; i < fwords.size();i++) {
-    console.log(i,test.getvvec(fwords, i)); // get from vector of strings
+for (let i = 0; i < fwords.size(); i++) {
+    console.log(i, test.getvvec(fwords, i)); // get from vector of strings
 }
-console.log('alpha 2 is ',test.getvvec(test.getvv(DATA,'alpha'),2)); // Get item 2 from vector of doubles or strings in item alpha in the DATA map
+console.log('alpha 2 is ', test.getvvec(test.getvv(DATA, 'alpha'), 2)); // Get item 2 from vector of doubles or strings in item alpha in the DATA map
+console.log('alpha 2 is ', test.getv(DATA, 'alpha')[2]); // Get item 2 from vector of doubles or strings in item alpha in the DATA map
+
+const tt=test.getv(DATA,'alpha');
+const DOT = (a) => {
+    let k = 0;
+    a.forEach(d => {
+        k += d * d;
+    });
+    return k;
+}
+console.log(tt);
+console.log(DOT(test.getv(DATA,'alpha')));
