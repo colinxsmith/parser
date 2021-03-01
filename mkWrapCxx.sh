72eb77ae19fb05cfe45bb89ec8695ce0358c7844
Colin <colinxsmith@gmail.com>
2021-03-01 19:22:20 +0000

    No need for hacks! Works with SWIG as is.

--- mkWrapCxx.sh
+++ mkWrapCxx.sh
@@ -1,4 +1,5 @@
 PATH=$PATH:/usr/bin
 swig=c:/SWIGcvs/SWIG/swig
 $swig -javascript -c++ -node -o parse_wrap.cxx libdata.i
-dos2unix make_12.1.sed && bash make_12.1.sed parse_wrap.cxx
+#No need to bother about make_12.1.sed anymore
+#dos2unix make_12.1.sed && bash make_12.1.sed parse_wrap.cxx
--- swigstep.sh
+++ swigstep.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 swig=~/SWIGcvs/SWIG/swig
 $swig -javascript -c++ -node -o parse_wrap.cxx libdata.i
-bash make_12.1.sed parse_wrap.cxx
+#No need to bother about make_12.1.sed anymore
+#bash make_12.1.sed parse_wrap.cxx
 #awk 'BEGIN{i=0;};{if(i++%2==0){print $1;}}' /SDrive/logFile.log | sed "/-----/d;/names/d"|tr '\n' ' ' >> checklog.js
